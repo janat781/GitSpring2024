0d3325aa786c7e8227e9030a6612862af2fb8ba9
Aika <jakudry04@gmail.com>
2024-08-09 19:51:50 +0600

    String1/ExtraEnd

---
+++ src/main/java/codingbat/string1/ExtraEnd.java
@@ -0,0 +1,10 @@
+package codingbat.string1;
+
+public class ExtraEnd {
+    public String extraEnd(String str) {
+        if (str.length() >= 2){
+            return str.substring(str.length()-2) + str.substring(str.length()-2) + str.substring(str.length()-2);
+        } return str;
+    }
+
+}
