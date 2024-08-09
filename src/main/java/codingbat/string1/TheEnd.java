e9c5767d71defb6d4cb9356076c584eda16582fe
Aika <jakudry04@gmail.com>
2024-08-09 20:26:31 +0600

    String1/TheEnd

---
+++ src/main/java/codingbat/string1/TheEnd.java
@@ -0,0 +1,10 @@
+package codingbat.string1;
+
+public class TheEnd {
+    public String theEnd(String str, boolean front) {
+        if (front == true){
+            return str.substring(0,1);
+        } return str.substring(str.length()- 1);
+    }
+
+}
