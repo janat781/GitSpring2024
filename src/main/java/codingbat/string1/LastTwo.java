1cee3036c770898203544a53a257b2ef5a7fa170
Aika <jakudry04@gmail.com>
2024-08-09 20:37:28 +0600

    String1/LastTwo

---
+++ src/main/java/codingbat/string1/LastTwo.java
@@ -0,0 +1,10 @@
+package codingbat.string1;
+
+public class LastTwo {
+    public String lastTwo(String str) {
+        if (str.length() > 1 ){
+            return str.substring(0, str.length()-2) + str.substring(str.length()-1) + str.substring(str.length()-2,str.length()-1);
+        }return str;
+    }
+
+}
