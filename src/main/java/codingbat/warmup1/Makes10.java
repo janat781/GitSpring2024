0b65d96cc1ec826ae33735a8528d163fc4127a5d
Aika <jakudry04@gmail.com>
2024-08-10 14:12:59 +0600

    Warmup1/Makes10

---
+++ src/main/java/codingbat/warmup1/Makes10.java
@@ -0,0 +1,15 @@
+package codingbat.warmup1;
+
+public class Makes10 {
+    public boolean makes10(int a, int b) {
+        if(a == 10 || b == 10){
+            return true;
+        }
+        if (a + b == 10){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
