098fdca37b6c691ce33fb2282d78ec5eb672676f
Aika <jakudry04@gmail.com>
2024-08-10 13:51:57 +0600

    Warmup1/BackAround

--- src/main/java/codingbat/warmup1/Front3.java
+++ src/main/java/codingbat/warmup1/Front3.java
@@ -1,2 +1,9 @@
-package codingbat.warmup1;public class Front3 {
+package codingbat.warmup1;
+
+public class Front3 {
+    public String front3(String str) {
+        if (str.length()>=3){
+            return str.substring(0,3) + str.substring(0,3)+ str.substring(0,3);
+        } return str+str+str;
+    }
 }
