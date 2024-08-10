68210ddc604fa3f8ec47a346297ed662620dd214
Aika <jakudry04@gmail.com>
2024-08-10 13:54:12 +0600

    Warmup1/StartHi

---
+++ src/main/java/codingbat/warmup1/StartHi.java
@@ -0,0 +1,9 @@
+package codingbat.warmup1;
+
+public class StartHi {
+    public boolean startHi(String str) {
+        if (str.startsWith("hi")){
+            return true;
+        }return false;
+    }
+}
