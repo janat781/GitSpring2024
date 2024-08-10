21a8c591909e21e0be4ca1b5bb77462523ba613d
Aika <jakudry04@gmail.com>
2024-08-10 13:45:11 +0600

    Warmup1/ParrotTrouble

---
+++ src/main/java/codingbat/warmup1/ParrotTrouble.java
@@ -0,0 +1,10 @@
+package codingbat.warmup1;
+
+public class ParrotTrouble {
+    public boolean parrotTrouble(boolean talking, int hour) {
+        if (talking &&(hour < 7 || hour > 20 )){
+            return true;
+        }
+        return false;
+    }
+}
