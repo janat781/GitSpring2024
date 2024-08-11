41cc2b522782fac9ce8d54cf186cf6a43991a5b7
Aika <jakudry04@gmail.com>
2024-08-11 23:53:18 +0600

    Array2/HaveThree

---
+++ src/main/java/codingbat/array2/HaveThree.java
@@ -0,0 +1,17 @@
+package codingbat.array2;
+
+public class HaveThree {
+    public boolean haveThree(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++){
+            if (nums[i] == 3){
+                count++;
+                if (i + 1 < nums.length && nums[i + 1] == 3){
+                    return false;
+                }
+            }
+        }
+        return count == 3;
+    }
+
+}
